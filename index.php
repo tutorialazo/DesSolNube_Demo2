72ae1da65e53fb1e34a5792e82ac56527b8eb509
odenfis <wmoncada@tecsup.edu.pe>
2023-03-29 16:16:12 -0500

    version 7

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
       <footer class="pt-4 my-md-5 pt-md-5 border-top">
         <div class="row">
           <div class="col-12 col-md">
-            <img class="mb-2" src="uncle_svgrepo-com.svg" alt="" width="24" height="24">
+            <img class="mb-2" src="https://www.svgrepo.com/show/508391/uncle.svg" alt="" width="24" height="24">
             <small class="d-block mb-3 text-muted">&copy; 2023-1</small>
           </div>
         </div>
