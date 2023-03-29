2180d3fc3be416995db0b89975b43e20b2c348d4
odenfis <wmoncada@tecsup.edu.pe>
2023-03-29 16:09:14 -0500

    version 6

--- index.php
+++ index.php
@@ -79,8 +79,8 @@
       <footer class="pt-4 my-md-5 pt-md-5 border-top">
         <div class="row">
           <div class="col-12 col-md">
-            <img class="mb-2" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
-            <small class="d-block mb-3 text-muted">&copy; 2017-2018</small>
+            <img class="mb-2" src="uncle_svgrepo-com.svg" alt="" width="24" height="24">
+            <small class="d-block mb-3 text-muted">&copy; 2023-1</small>
           </div>
         </div>
       </footer>
