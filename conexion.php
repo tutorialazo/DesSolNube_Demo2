ae6340ad15126abc89f1130dbc986e889e6ac7fc
walter <wmoncada@tecsup.edu.pe>
2023-09-05 10:10:30 -0500

    sesion2_v2_connect

--- conexion.php
+++ conexion.php
@@ -2,11 +2,11 @@
 
 	function conexion(){
 
-	$host = "host=xxxx";
-	$port = "port=xxxx";
-	$dbname = "dbname=xxxx";
-	$user = "user=xxxx";
-	$password = "password=xxxx";
+	$host = "host=containers-us-west-173.railway.app";
+	$port = "port=6928";
+	$dbname = "dbname=railway";
+	$user = "user=postgres";
+	$password = "password=dS8fvl7EvlUbyCUI9oaH";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
