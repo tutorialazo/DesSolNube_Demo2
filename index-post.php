0ffe98fc64c409fe6c200167a8b7b697d93023c1
Odenfis <105258251+Odenfis@users.noreply.github.com>
2024-04-05 11:36:27 -0500

    change2

--- index-post.php
+++ index-post.php
@@ -1,18 +1,18 @@
 <?php
 include("conexion.php");
 $con = conexion();
-
+/*
 $doc = $_POST["doc"];
 $nom = $_POST["nom"];
 $ape = $_POST["ape"];
 $dir = $_POST["dir"];
-$cel = $_POST["cel"];
+$cel = $_POST["cel"];*/
 
-$doc = pg_escape_string($doc);
-$nom = pg_escape_string($nom);
-$ape = pg_escape_string($ape);
-$dir = pg_escape_string($dir);
-$cel = pg_escape_string($cel);
+$doc = pg_escape_literal($_POST["doc"]);
+$nom = pg_escape_literal($_POST["nom"]);
+$ape = pg_escape_literal($_POST["ape"]);
+$dir = pg_escape_literal($_POST["dir"]);
+$cel = pg_escape_literal($_POST["cel"]);
 
 $sql = "insert into persona values(default,'$doc','$nom','$ape','$dir','$cel')";
 pg_query($con, $sql);
