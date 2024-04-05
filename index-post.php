751760184dd6fb3627803d63031477d4a1670494
Odenfis <105258251+Odenfis@users.noreply.github.com>
2024-04-05 11:33:22 -0500

    change1

--- index-post.php
+++ index-post.php
@@ -8,6 +8,12 @@ $ape = $_POST["ape"];
 $dir = $_POST["dir"];
 $cel = $_POST["cel"];
 
+$doc = pg_escape_string($doc);
+$nom = pg_escape_string($nom);
+$ape = pg_escape_string($ape);
+$dir = pg_escape_string($dir);
+$cel = pg_escape_string($cel);
+
 $sql = "insert into persona values(default,'$doc','$nom','$ape','$dir','$cel')";
 pg_query($con, $sql);
 
