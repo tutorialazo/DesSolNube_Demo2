ed05f250bae72aeccd21305571d0f306f536feec
Sandy28LM <sandy28041127@gmail.com>
2024-04-11 11:45:06 -0500

    NuevoListar

--- listar_SandyLeiva.php
+++ listar_SandyLeiva.php
@@ -8,35 +8,48 @@
     <meta name="author" content="">
     <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
 
-    
-
     <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/pricing/">
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    
+
+    <style>
+      h1 {
+        font-weight: bold;
+      }
+      .card-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+      }
+
+      .card {
+        box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
+      }
+    </style>
+
   </head>
 
   <body>
 
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-      <h5 class="my-0 mr-md-auto font-weight-normal"><img src="index2.png" style="width: 30px; position: absolute;"> <span style="position: relative; left: 35px;">Index</span></h5>
-      <nav class="my-2 my-md-0 mr-md-3">
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom box-shadow">
+      <h5 class="my-0 mr-md-auto font-weight-normal ml-3"><img src="index2.png" style="width: 30px; position: absolute;"> <span style="position: relative; left: 35px;">Index</span></h5>
+      <nav class="my-2 my-md-0 mr-md-3 navbar">
         <a class="p-2 text-dark" href="#">Registrar</a>
         <a class="p-2 text-dark" href="#">Actualizar</a>
-        <a class="p-2 text-dark" href="#">Eliminar</a>
+        <a class="p-2 text-dark mr-3" href="#">Eliminar</a>
       </nav>
     </div>
 
     <div class="container px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-      <h1 class="display-4">Registrando datos with Railway</h1>
+      <h1 class="display-4 text-info">Registrando datos with Railway</h1>
       <p class="lead">PostgreSQL + PHP</p>
     </div>
 
     <div class="container">
       <div class="card">
         <div class="card-body">
-          <!-- Formulario para registrar nuevas personas -->
           <form autocomplete="off" action="index-post.php" method="post">
             <div class="row">
               <div class="col-sm-4 col-4">
@@ -72,7 +85,7 @@
                 </div>
               </div>
             </div>
-            <input type="submit" class="btn btn-primary float-right" value="Registrar">
+            <input type="submit" class="btn btn-info float-right" value="Registrar">
           </form>
         </div>
       </div>
@@ -80,10 +93,10 @@
       <!-- Tabla para mostrar los datos de las personas -->
       <div class="card mt-5">
         <div class="card-body">
-          <h2 class="card-title">Lista de Personas</h2>
+          <h2 class="card-title text-center mb-4 text-info">Lista de Personas Registradas</h2>
           <div class="table-responsive">
-            <table class="table table-striped">
-              <thead>
+            <table class="table table-striped text-center">
+                <thead class="table-info">
                 <tr>
                   <th>Nro Documento</th>
                   <th>Nombre</th>
@@ -94,19 +107,12 @@
               </thead>
               <tbody>
                 <?php
-                // Incluir el archivo de conexión
                 include("conexion.php");
-
-                // Establecer la conexión
                 $con = conexion();
-
-                // Consulta SQL para seleccionar todas las filas de la tabla "persona"
                 $sql = "SELECT * FROM persona";
 
                 // Ejecutar la consulta
                 $resultado = pg_query($con, $sql);
-
-                // Comprobar si la consulta se realizó con éxito
                 if ($resultado) {
                   // Recorrer los resultados y mostrar cada fila en la tabla
                   while ($fila = pg_fetch_assoc($resultado)) {
@@ -119,7 +125,6 @@
                     echo "</tr>";
                   }
                 } else {
-                  // Mostrar un mensaje de error si la consulta falla
                   echo "<tr><td colspan='5'>Error al ejecutar la consulta SQL: " . pg_last_error($con) . "</td></tr>";
                 }
 
@@ -133,7 +138,7 @@
       </div>
     </div>
 
-    <footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <footer class="pt-4 text-center">
       <div class="row">
         <div class="col-12 col-md">
           <img class="mb-2" src="https://www.svgrepo.com/show/508391/uncle.svg" alt="" width="24" height="24">
@@ -142,9 +147,9 @@
       </div>
     </footer>
 
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+  </body>
+</html>
