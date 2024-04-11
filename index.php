2edd94f711c2410d7009bac6dec9ad2528e7b3c8
Odenfis <105258251+Odenfis@users.noreply.github.com>
2024-04-11 11:31:59 -0500

    Update index.php
    style

--- index.php
+++ index.php
@@ -14,9 +14,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <style>
-      body {
-        background:black;
-      }
       h1 {
         font-weight: bold;
       }
@@ -25,6 +22,7 @@
         justify-content: center;
         align-items: center;
         height: 100%;
+        background-color: black;
       }
 
       .card {
