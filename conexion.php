4a9c85f16eae70611745ecf72e11914feddeccda
Sandy28LM <sandy28041127@gmail.com>
2024-04-11 11:12:45 -0500

    Conexion

--- conexion.php
+++ conexion.php
@@ -2,12 +2,12 @@
 	function conexion(){
 
 	$host = "host=monorail.proxy.rlwy.net";
-	$port = "port=41590";
-	$dbname = "dbname=Postgres";
+	$port = "port=21925";
+	$dbname = "dbname=railway";
 	$user = "user=postgres";
-	$password = "oYoVgzNsOBPQiALcvuRugAtxyRVDSVSR";
+	$password = "YdeeqpIGHxnFVEurKSncMEaXGpAzlHPY";
 
-	$db = pg_connect=("$host $port $dbname $user $password");
+	$db = pg_connect("$host $port $dbname $user $password");
 
 	return $db;
 }
