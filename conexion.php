135b2f773129c5b2b777ce1996c54ca87f53bea4
Odenfis <105258251+Odenfis@users.noreply.github.com>
2024-04-11 10:31:01 -0500

    Update conexion.php
    new4

--- conexion.php
+++ conexion.php
@@ -1,17 +1,13 @@
 <?php
 	function conexion(){
 
-	$host = "host=monorail.proxy.rlwy.net";
+	$host = "monorail.proxy.rlwy.net";
 	$port = "port=47760";
-	$dbname = "dbname=railway";
+	$dbname = "dbname=Postgres";
 	$user = "user=postgres";
-	$password = "password=JXBDNUAXDEitXULmBlnqrAzGTnKYOJrI";
+	$password = "JXBDNUAXDEitXULmBlnqrAzGTnKYOJrI";
 
 	$db = pg_connect("$host $port $dbname $user $password");
-
-	if (!$db) {
-    	die("Error al conectar con PostgreSQL: " . pg_last_error());
-	}
 
 	return $db;
 }
