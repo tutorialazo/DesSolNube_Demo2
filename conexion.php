adbca43e8c39e60ecf034d93142c41868f06cce2
Odenfis <105258251+Odenfis@users.noreply.github.com>
2024-04-11 10:46:47 -0500

    Update conexion.php
    new8

--- conexion.php
+++ conexion.php
@@ -7,7 +7,7 @@
 	$user = "user=postgres";
 	$password = "oYoVgzNsOBPQiALcvuRugAtxyRVDSVSR";
 
-	$db = pg_connect=("$host $port $dbname $user $password");
+	$db = pg_connect("$host $port $dbname $user $password");
 
 	return $db;
 }
