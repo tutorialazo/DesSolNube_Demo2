621338c35cc7f08667735d64dacb156daec2c06d
Odenfis <105258251+Odenfis@users.noreply.github.com>
2024-04-11 16:19:21 -0500

    frontP

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
     </div>
 
     <div class="container px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-      <h1 class="display-4 text-info">Registrando datos with Railway - 2024</h1>
+      <h1 class="display-4 text-info">Registrando datos with Railway</h1>
       <p class="lead">PostgreSQL + PHP</p>
     </div>
 
