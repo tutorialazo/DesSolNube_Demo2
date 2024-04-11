60c368b26833fd8020859c65c42a547e32fa5e28
Odenfis <105258251+Odenfis@users.noreply.github.com>
2024-04-11 16:29:12 -0500

    rosadoP

--- index.php
+++ index.php
@@ -108,7 +108,7 @@
       <div class="row">
         <div class="col-12 col-md">
           <img class="mb-2" src="https://www.svgrepo.com/show/508391/uncle.svg" alt="" width="24" height="24">
-          <small class="d-block mb-3 text-muted">&copy; 2023-1</small>
+          <small class="d-block mb-3 text-muted">&copy; 2024-1</small>
         </div>
       </div>
     </footer>
